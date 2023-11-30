204bda289102f8b381a8b3118dd2ca7bcf1e8c3b
SnakyMz <Soban.syeed@outlook.com>
2023-12-01 04:35:28 +0530

    Fix linters

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,11 +7,11 @@ class Api::V1::UsersController < ApplicationController
   # POST /users or /users.json
   def create
     @user = User.new(user_params)
-      if @user.save
-        render json: :show
-      else
-        render json: @user.errors
-      end
+    if @user.save
+      render json: :show
+    else
+      render json: @user.errors
+    end
   end
 
   private
