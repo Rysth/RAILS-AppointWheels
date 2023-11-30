8995e4d832b745a59bff90866b837520a5fc802c
Rebelzob <karimdavid07@gmail.com>
2023-11-30 19:47:12 +0000

    Add nested resources for rentals in users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, only: []
+      resources :users, only: [] do
+        resources :rentals, only: [:index, :create]
       resources :cars
     end
   end
