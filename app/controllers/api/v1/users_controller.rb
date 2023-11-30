486603083b3ab2226a3919c5b4dc155be028de7b
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-11-30 13:13:38 -0300

    add user model

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # POST /users or /users.json
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Only allow a list of trusted parameters through.
+    def user_params
+      params.require(:user).permit(:name, :password)
+    end
+end
