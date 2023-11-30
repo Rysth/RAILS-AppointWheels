2a3c4214355c77bf7c8bc923ac123280bfa8e1a9
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-11-30 13:14:21 -0300

    add user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  
+  namesapce :api do
+    namesapce :v1 do
+      resources :users, only: []
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
