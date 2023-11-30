8f606b93eaa1762286cec5e199dfbfcdf5ae067c
Rebelzob <karimdavid07@gmail.com>
2023-11-30 17:35:40 +0000

    Add seed data for rentals

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,13 @@ end
     name: Faker::Name.name,
   )
 end
+
+10.times do
+  rental = Rental.create({
+    total_price: Faker::Number.decimal(l_digits: 2),
+    start_date: Faker::Date.between(from: 1.year.ago, to: Date.today),
+    end_date: Faker::Date.between(from: Date.today + 1, to: 1.year.from_now),
+    car_id: Faker::Number.between(from: 1, to: 10),
+    user_id: Faker::Number.between(from: 1, to: 10)
+  })
+end
