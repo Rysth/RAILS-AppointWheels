b4f75bb4b905e0bac3d55231a1b3ba31cb33877c
Rebelzob <karimdavid07@gmail.com>
2023-11-30 17:35:32 +0000

    Update schema.rb with new table 'rentals'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_30_162114) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_30_170945) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,10 +24,24 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_30_162114) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "rentals", force: :cascade do |t|
+    t.float "total_price", null: false
+    t.date "start_date", null: false
+    t.date "end_date", null: false
+    t.bigint "car_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_rentals_on_car_id"
+    t.index ["user_id"], name: "index_rentals_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "rentals", "cars"
+  add_foreign_key "rentals", "users"
 end
