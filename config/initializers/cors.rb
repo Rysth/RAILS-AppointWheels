d8b87c7f792c177873c9be45ef906f41ea3d2184
John Palacios <johnpalacios.t@gmail.com>
2023-12-05 10:40:18 -0500

    Update CORS policy to handle request with authorizations.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'  # Add your frontend URL
+    origins 'http://localhost:3000'  # Add your frontend URL
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      methods: [:get, :post, :put, :patch, :delete, :options, :head], 
+      credentials: true,
       expose: [:Authorization]
   end
 end
