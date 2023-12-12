b1b1ecfc8512f1706de09ba8f55bc74e5c67e7a5
SnakyMz <Soban.syeed@outlook.com>
2023-12-12 20:14:06 +0530

    Add role to users for authorization

--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_30_210553) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "role", default: "user"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
