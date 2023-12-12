373dc22482e16f5ee9c5f99935871e169dad92bf
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-12-12 12:22:07 -0300

    add ability.rb

---
+++ app/models/ability.rb
@@ -0,0 +1,15 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    return unless user.present?
+
+    can :read, :all
+    can :manage, Rental
+
+    return unless user.admin?
+
+    can :manage, :all
+
+  end
+end
