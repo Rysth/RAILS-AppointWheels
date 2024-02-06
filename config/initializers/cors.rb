dd375350605d985570cac3cec5b880bf32aeea0c
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-02-06 18:53:54 -0300

    Update CORS origins in config/initializers/cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'origins 'https://appointwheels.onrender.com', 'https://appoint-wheels.onrender.com'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head], 
